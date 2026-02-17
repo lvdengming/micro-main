bc2de2d9278612e785282ede94697d2906f213b7
lvdengming <lvdengming@foxmail.com>
2026-02-17 10:40:46 +0800

    feat: 完善微前端配置

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@
  * @Author: lvdengming@foxmail.com
  * @Date: 2026-01-25 18:44:20
  * @LastEditors: lvdengming@foxmail.com
- * @LastEditTime: 2026-02-16 22:18:06
+ * @LastEditTime: 2026-02-17 09:57:48
  */
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { start } from 'qiankun';
 import { RouteName } from './app.routes';
 
 @Component({
@@ -16,6 +17,13 @@ import { RouteName } from './app.routes';
   standalone: true,
   imports: [RouterOutlet, CommonModule, RouterModule],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public readonly RouteName = RouteName;
+
+  ngOnInit(): void {
+    start({
+      sandbox: true,
+      prefetch: 'all',
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
+/*
+ * @Author: lvdengming@foxmail.com
+ * @Date: 2026-01-25 18:44:20
+ * @LastEditors: lvdengming@foxmail.com
+ * @LastEditTime: 2026-02-17 10:27:02
+ */
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { registerMicroApps } from 'qiankun';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const getMicroApp = (name: string) => ({
+  name,
+  entry: `//localhost:8001`,
+  container: '#micro-app',
+  activeRule: (location: Location) => location.pathname === `/${name}`,
+});
+
+registerMicroApps([getMicroApp('angular-home'), getMicroApp('angular-detail')]);
+
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
