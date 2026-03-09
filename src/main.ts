242377ada370885dfd73cf446c91fa6f71700f7b
lvdengming <lvdengming@foxmail.com>
2026-03-10 01:47:06 +0800

    feat: 完善微前端部署配置

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: lvdengming@foxmail.com
  * @Date: 2026-01-25 18:44:20
  * @LastEditors: lvdengming@foxmail.com
- * @LastEditTime: 2026-02-17 10:27:02
+ * @LastEditTime: 2026-03-10 01:18:33
  */
 import { bootstrapApplication } from '@angular/platform-browser';
 import { registerMicroApps } from 'qiankun';
@@ -12,7 +12,7 @@ import { environment } from './environments/environment';
 
 const getMicroApp = (name: string) => ({
   name,
-  entry: environment.production ? 'http://localhost:8001/index.html' : '//localhost:8001',
+  entry: environment.production ? 'http://8.141.84.169:8001/index.html' : '//localhost:8001',
   container: '#micro-app',
   activeRule: (location: Location) => location.pathname === `/${name}`,
 });
